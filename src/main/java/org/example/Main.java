192921881a0e08de890c260c2284a7aa17c4be84
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-08-14 16:10:28 +0300

    Resolved merge conflict

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
+        User user = new User("Name", "Surname", (short) 21);
         Product product = new Product("Apple", BigDecimal.valueOf(20));
+        System.out.println("Created user!");
         System.out.println("Created product!");
     }
 }

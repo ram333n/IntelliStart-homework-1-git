648085555b75992b556380361aba37cfbf832f17
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-08-14 15:56:04 +0300

    Added examples of User and Product usage
    Resolved merge conflict

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user = new User("Name", "Surname", (short) 21);
+        Product product = new Product("Apple", BigDecimal.valueOf(20));
+        System.out.println("Created user!");
+        System.out.println("Created product!");
     }
 }

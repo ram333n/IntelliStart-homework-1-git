4532dfaad772dce70d523139d91806a2f77c307d
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-08-14 15:56:04 +0300

    Added example of Product class usage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product product = new Product("Apple", BigDecimal.valueOf(20));
+        System.out.println("Created product!");
     }
 }

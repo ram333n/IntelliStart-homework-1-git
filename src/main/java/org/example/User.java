948b0f2768f30519c1129a0a160d36362b71f932
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-08-14 15:06:24 +0300

    2: Added ctor for user class

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -4,4 +4,10 @@ public class User {
     private String name;
     private String surname;
     private Short age;
+
+    public User(String name, String surname, Short age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
 }

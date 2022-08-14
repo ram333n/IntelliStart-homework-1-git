c7903c966562a892d36f1ed26107d92cd6f2ff54
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-08-14 15:23:31 +0300

    2: Added lombok annotations for setters, getters, all args ctor

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,13 +1,14 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class User {
     private String name;
     private String surname;
     private Short age;
-
-    public User(String name, String surname, Short age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-    }
 }

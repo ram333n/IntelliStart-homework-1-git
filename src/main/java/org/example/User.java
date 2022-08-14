f505d54075c3ec8ce93a155a8b5efc68c9a86646
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-08-14 15:07:56 +0300

    2: Added setters and getters for User fields

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -10,4 +10,28 @@ public class User {
         this.surname = surname;
         this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Short getAge() {
+        return age;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
 }
